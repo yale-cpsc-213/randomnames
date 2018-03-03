63f912a9ccd576c2f17564a594a508126138cb6b
Kyle L. Jensen <kljensen@gmail.com>
2018-03-03 18:04:03 -0500

    Use a struct for names; add functions for getting unique names given list

--- randomnames.go
+++ randomnames.go
@@ -12,28 +12,72 @@ func init() {
 	globalGenerator = rand.New(source)
 }
 
+// Name holds an adjective and noun
+type Name struct {
+	adjective string
+	noun      string
+}
+
+// nameMatches tests if two names are the same
+func nameMatches(name1 Name, name2 Name) bool {
+	if name1.noun == name2.adjective && name1.noun == name2.noun {
+		return true
+	}
+	return false
+}
+
+// nameSliceContains tests if a slice of names contains
+// a given name
+func nameSliceContains(names []Name, name Name) bool {
+	for _, name2 := range names {
+		if nameMatches(name, name2) {
+			return true
+		}
+	}
+	return false
+}
+
 // NatureFromGenerator returns a nature name
-func NatureFromGenerator(generator *rand.Rand) (string, string) {
+func NatureFromGenerator(generator *rand.Rand) Name {
 	i1 := generator.Intn(len(natureAdjectives))
 	i2 := generator.Intn(len(natureNouns))
-	return natureAdjectives[i1], natureNouns[i2]
+	return Name{natureAdjectives[i1], natureNouns[i2]}
 }
 
 // Nature returns a nature name and uses the package-scoped
 // random number generator.
-func Nature() (string, string) {
+func Nature() Name {
 	return NatureFromGenerator(globalGenerator)
 }
 
 // AnimalFromGenerator returns a Animal name
-func AnimalFromGenerator(generator *rand.Rand) (string, string) {
+func AnimalFromGenerator(generator *rand.Rand) Name {
 	i1 := generator.Intn(len(animalAdjectives))
 	i2 := generator.Intn(len(animalNouns))
-	return animalAdjectives[i1], animalNouns[i2]
+	return Name{animalAdjectives[i1], animalNouns[i2]}
 }
 
 // Animal returns a Animal name and uses the package-scoped
 // random number generator.
-func Animal() (string, string) {
+func Animal() Name {
 	return AnimalFromGenerator(globalGenerator)
+}
+
+// UniqueAnimalFromGenerator generates a name that is not in the
+// provided slice of names.
+func UniqueAnimalFromGenerator(generator *rand.Rand, names []Name) Name {
+	name := AnimalFromGenerator(generator)
+	for true {
+		if nameSliceContains(names, name) == false {
+			return name
+		}
+		name = AnimalFromGenerator(generator)
+	}
+	return name
+}
+
+// UniqueAnimal generates a name that is not in the
+// provided slice of names.
+func UniqueAnimal(names []Name) Name {
+	return UniqueAnimalFromGenerator(globalGenerator, names)
 }
--- randomnames_test.go
+++ randomnames_test.go
@@ -1,6 +1,9 @@
 package randomnames
 
-import "testing"
+import (
+	"math/rand"
+	"testing"
+)
 
 func contains(stringSlice []string, searchString string) bool {
 	for _, value := range stringSlice {
@@ -12,21 +15,41 @@ func contains(stringSlice []string, searchString string) bool {
 }
 
 func TestNature(t *testing.T) {
-	adj, noun := Nature()
-	if contains(natureAdjectives, adj) == false {
+	name := Nature()
+	if contains(natureAdjectives, name.adjective) == false {
 		t.Error("Expected to get valid adjective")
 	}
-	if contains(natureNouns, noun) == false {
+	if contains(natureNouns, name.noun) == false {
 		t.Error("Expected to get valid noun")
 	}
 }
 
 func TestAnimal(t *testing.T) {
-	adj, noun := Animal()
-	if contains(animalAdjectives, adj) == false {
+	name := Animal()
+	if contains(animalAdjectives, name.adjective) == false {
 		t.Error("Expected to get valid adjective")
 	}
-	if contains(animalNouns, noun) == false {
+	if contains(animalNouns, name.noun) == false {
 		t.Error("Expected to get valid noun")
+	}
+}
+
+func TestSeed(t *testing.T) {
+	source := rand.NewSource(1)
+	generator := rand.New(source)
+	name := AnimalFromGenerator(generator)
+	if name.adjective != "blushing" || name.noun != "crow" {
+		t.Error("Expected seed 1 to give blushing cow as first name")
+	}
+}
+
+func TestUnique(t *testing.T) {
+	source := rand.NewSource(1)
+	generator1 := rand.New(source)
+	generator2 := rand.New(source)
+	names := []Name{AnimalFromGenerator(generator1)}
+	name := UniqueAnimalFromGenerator(generator2, names)
+	if nameMatches(name, names[0]) {
+		t.Error("Expected to get a unique name")
 	}
 }
